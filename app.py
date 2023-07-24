f3b22d7d12e84bc1ff7d4bcb6ab0c6289a7b32ae
NandanReddyP <parnapalli2004@gmail.com>
2023-07-24 10:43:06 +0530

    some good changes

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 print('welcome to monty hall')
 from flask import Flask, render_template, redirect
+
+app = Flask('__main__', template_folder='templates')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+@app.route('/play', methods=['GET', 'POST'])
+def play():
+    pass
+    return render_template('first.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
